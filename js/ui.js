1c14b067f03588495f577ef1c24ebf2a89577f93
Jahazel Sanchez <jahazelsanchez1@gmail.com>
2026-02-21 15:43:15 -0600

    Update UI to Github inspired dark theme

--- js/ui.js
+++ js/ui.js
@@ -2,12 +2,15 @@ export function renderMovies(movies) {
   const movieGrid = document.getElementById("movie-grid");
   let movieCards = movies
     .map(({ Poster, Title, Year, imdbID }) => {
-      return `<div id="${imdbID}" class="movie-card">
-      <img src="${Poster !== "N/A" ? Poster : "https://placehold.co/300x445?text=No+Poster"}" alt="movie poster" onerror="this.src='https://placehold.co/300x445?text=No+Poster'">
-        <p class="title">${Title}</p>
-        <p class="release-year">${Year}</p>
-        <button class="fav-btn">Favorite</button>
-       </div>`;
+      return `
+      <div id="${imdbID}" class="movie-card">
+        <img src="${Poster !== "N/A" ? Poster : "https://placehold.co/300x445?text=No+Poster"}" alt="movie poster" onerror="this.src='https://placehold.co/300x445?text=No+Poster'">
+        <div class="info-container">
+            <p class="title">${Title}</p>
+            <p class="release-year">${Year}</p>
+            <button class="fav-btn">Favorite</button>
+        </div>  
+      </div>`;
     })
     .join(" ");
 
